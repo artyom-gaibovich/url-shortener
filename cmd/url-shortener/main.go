080bda7b5401f0686ce2b443744f2f6f48496882
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-06-25 16:09:52 +0300

    project has been configurated

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
 func main() {
-	// TODO: init config : cleanenv
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 	// TODO: init logger : slog
 	// TODO: init storage : sqlite
-	// TODO: init router :
+	// TODO: init router : "chi, render"
 	// TODO : run server
+
 }
